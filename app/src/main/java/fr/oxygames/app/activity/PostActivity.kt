8d4d74ec0b31e781dbf62b531d11b54256f71c35
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-12-05 05:45:14 +0100

    update Pick up images add post Blog

--- app/src/main/java/fr/oxygames/app/activity/PostActivity.kt
+++ app/src/main/java/fr/oxygames/app/activity/PostActivity.kt
@@ -1,19 +1,17 @@
 package fr.oxygames.app.activity
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.storage.StorageReference
 import fr.oxygames.app.databinding.ActivityPostBinding
-import fr.oxygames.app.model.Users
-
+import org.jetbrains.anko.longToast
 
 class PostActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostBinding
@@ -22,15 +20,7 @@ class PostActivity : AppCompatActivity() {
     private var postTitle: EditText? = null
     private var postDesc: EditText? = null
     private var submitBtn: Button? = null
-
-    private var images: ArrayList<Uri?>? = null
-    private var position = 0
-    private val pickImagesCode = 0
-
-    private var usersReference: DatabaseReference? = null
-    private var firebaseUser: FirebaseUser? = null
-    private var user: Users? = null
-    private var storageRef: StorageReference? = null
+    private var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,90 +32,76 @@ class PostActivity : AppCompatActivity() {
         //usersReference = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
         //storageRef = FirebaseStorage.getInstance().reference.child("User Images")
 
-        val selectImage = binding.imageSelect
+        selectImage = binding.imageSelect
         postTitle = binding.titleField
         postDesc = binding.descField
-        val submitBtn = binding.submitBtn
+        submitBtn = binding.submitBtn
 
-        images = ArrayList()
-
-        selectImage.setOnClickListener {
-            pickImage()
+        selectImage!!.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                    // permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    // show popup to request runtime permission
+                    requestPermissions(permissions, PermissionCode)
+                }
+                else {
+                    pickImageFromGallery()
+                }
+            }
+            else {
+                pickImageFromGallery()
+            }
         }
 
-        submitBtn.setOnClickListener {
-
+        submitBtn!!.setOnClickListener {
+            startPosting()
         }
     }
 
+    private fun startPosting() {
+        longToast("send message")
+    }
+
     // pickup image
-     private fun pickImage() {
+     private fun pickImageFromGallery() {
         val intent = Intent()
-        intent.type = "image/*"
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Image(s)"), pickImagesCode)
+        intent.type = "image/*"
+        startActivityForResult(intent, ImagePickCode)
+    }
+
+    companion object {
+        // image pick code
+        private const val ImagePickCode = 1
+        // Permission code
+        private const val PermissionCode = 1001
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            PermissionCode -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery()
+                }
+                else{
+                    longToast("Permission denied")
+                }
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == pickImagesCode) {
-            if (resultCode == Activity.RESULT_OK) {
-                if (data!!.clipData != null) {
-                    val count = data.clipData!!.itemCount
-                    for (i in 0 until count) {
-                        val imageUri = data.clipData!!.getItemAt(i).uri
-                        images!!.add(imageUri)
-                    }
-                    selectImage!!.setImageURI(images!![0])
-                    position = 0
-                }
-                else {
-                    val imageUri = data.data
-                    selectImage!!.setImageURI(imageUri)
-                    position = 0
-                }
-            }
+        if (requestCode == ImagePickCode && resultCode == RESULT_OK ) {
+            imageUri = data!!.data
+            selectImage!!.setImageURI(imageUri)
         }
     }
-
-/*
-    private fun uploadImageToDatabase() {
-        val progressBar = ProgressDialog(applicationContext)
-        progressBar.setMessage("image is uploading, please wait....")
-        progressBar.show()
-
-        if (imageUri != null)
-        {
-            val fileRef = storageRef!!.child(System.currentTimeMillis().toString() + ".jpg")
-            val uploadTask: StorageTask<*>
-            uploadTask = fileRef.putFile(imageUri)
-            uploadTask.continueWithTask(Continuation <UploadTask.TaskSnapshot, Task<Uri>>{ task ->
-                if(!task.isSuccessful){
-                    task.exception?.let {
-                        throw it
-                    }
-                }
-                return@Continuation fileRef.downloadUrl
-            }).addOnCompleteListener { task ->
-                if (task.isSuccessful)
-                {
-                    val downloadUrl = task.result
-                    val url = downloadUrl.toString()
-
-                    if (coverChecker == "cover")
-                    {
-                        val mapCoverImg = HashMap<String, Any>()
-                        mapCoverImg["cover"] = url
-                        usersReference!!.updateChildren(mapCoverImg)
-                        coverChecker = ""
-                    }
-
-                    progressBar.dismiss()
-                }
-            }
-        }
-    }
-*/
 }
